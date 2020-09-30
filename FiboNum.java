fe52443c2d9ce19e985d105e0b145d3ce60e2ecf
sandipan08 <sandipan.sau08@gmail.com>
2020-09-30 23:55:42 +0530

    Check 1st commit

--- FiboNum.java
+++ FiboNum.java
@@ -1,10 +1,21 @@
 import java.utill.*;
 class FiboNum{
+	
 	public static void main(String s[]){
-	
+		Scanner sc=new Scanner(System.in);
+		int n,i,t1=0,t2=1,nextitem;
+		System.out.println("Enter the number u generate ");
+		n=sc.nextInt();
+		
+		for(i=0;i<=n;i++)
+		{
+			System.out.println(t1);
+			nextitem=t1+t2;
+			t1=t2;
+			t2=nextitem;
+		}
+
 	}
-	Static int fibo(int n){
-	
-	}
+
 }	
 	
