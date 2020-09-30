3ff8f10a974d3c123a01adeea074b8a29f68db63
sandipan08 <sandipan.sau08@gmail.com>
2020-10-01 00:48:56 +0530

    Check amstrong

---
+++ Armstrong01.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Armstrong01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the number ");
+		int a=scan.nextInt();
+		String s=Integer.toString(a);
+		System.out.println(s.length());
+		
+
+	}
+
+}
