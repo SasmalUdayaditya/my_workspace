fa991870deec0a629d317842e0af6e7711dd00d2
Samrat003 <souvik.samrat1998@gmail.com>
2020-10-02 23:59:59 +0530

    initial

---
+++ 01_basic.js
@@ -0,0 +1,8 @@
+console.log('helo world');
+var fristname='Souvik'
+console.log(fristname);
+var age=4;
+console.log(age);
+// reserved keyword can not use like this
+// var if=4
+//console.log(if);
